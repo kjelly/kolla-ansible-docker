285460ef80d605c09f3ba5422c686d43339e10b9
Kuo-tung Kao <ya790206@gmail.com>
2017-03-22 15:26:36 +0000

    add color support

--- exec.sh
+++ exec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#docker exec -it kolla-ansible-docker /bin/bash
-docker exec -it kolla-ansible-docker /usr/bin/fish
+docker exec -it kolla-ansible-docker env TERM=xterm script -q -c "/bin/bash" /dev/null
+#docker exec -it kolla-ansible-docker /usr/bin/fish
