ddf5a5f1c309b99d85fdc74d2a3196947ff83a75
Kuo-tung Kao <kjellytw@gmail.com>
2019-05-16 16:36:00 +0800

    update _run script

--- _run.sh
+++ _run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 NAME=$1
+TAG=$2
 shift
 if [[ -z "${NAME// }" ]]
 then
   echo "請提供一個參數，作為名字"
   exit
 fi
+if [[ -z "${TAG// }" ]]
+then
+  TAG=latest
+fi
+
 sudo mkdir -p /etc/$NAME-ansible-docker
 sudo mkdir -p /etc/$NAME
-docker run --restart=always --privileged -d --net=host --name $NAME-ansible-docker -v /etc/$NAME:/etc/kolla -v /etc/$NAME-ansible-docker:/etc/kolla-ansible-docker -v /root/kolla-ansible-container-data:/root kolla-ansible-docker:$2
+docker run --restart=always --privileged -d --net=host --name $NAME-ansible-docker -v /etc/$NAME:/etc/kolla -v /etc/$NAME-ansible-docker:/etc/kolla-ansible-docker -v /root/kolla-ansible-container-data:/root kolla-ansible-docker:$TAG
