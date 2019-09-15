765a03485d6f5192121e1c98cb283e31846baf0a
Kuo-tung Kao <kjellytw@gmail.com>
2019-09-15 16:55:53 +0800

    Update

--- _exec.sh
+++ _exec.sh
@@ -6,5 +6,4 @@ then
   echo "請提供一個參數，作為名字"
   exit
 fi
-sudo docker exec -it $NAME-ansible-docker env TERM=xterm-256color script -q -c "/bin/bash" /dev/null
-#docker exec -it kolla-ansible-docker /usr/bin/fish
+sudo docker exec --detach-keys="ctrl-\,q" -it --env COLUMNS=`tput cols` --env LINES=`tput lines` --env TERM=xterm-256color $NAME-ansible-docker bash
